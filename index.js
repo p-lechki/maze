738f7dacfde0c10955dae6747207b4e30581d048
Paweł Lechki <p.lechki@icloud.com>
2020-01-01 19:25:42 +0100

    Init maze

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -18,13 +18,6 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse: Mouse.create(render.canvas)
-	})
-);
-
 // Walls
 const walls = [
 	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
@@ -33,20 +26,3 @@ const walls = [
 	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
-
-// Random Shapes
-
-for (let i = 0; i < 50; i++) {
-	if (Math.random() > 0.5) {
-		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-	} else {
-		World.add(
-			world,
-			Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-				render: {
-					fillStyle: 'green'
-				}
-			})
-		);
-	}
-}
