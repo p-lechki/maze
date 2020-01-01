121d1ba1263415edf70b050ee630850b8f1bcb61
Paweł Lechki <p.lechki@icloud.com>
2020-01-01 21:41:37 +0100

    Add ball

--- index.js
+++ index.js
@@ -141,7 +141,12 @@ verticals.forEach((row, rowIndex) => {
 	});
 });
 
+// Goal
 const goal = Bodies.rectangle(width - unitLength / 2, height - unitLength / 2, unitLength * 0.7, unitLength * 0.7, {
 	isStatic: true
 });
 World.add(world, goal);
+
+// Ball
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+World.add(world, ball);
